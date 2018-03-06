4191fbb1e80e2d443922bcf89cca7d18f93d9c1c
MarcinKosinski <m.p.kosinski@gmail.com>
2018-03-06 13:45:49 +0100

    censoring plots and normalized plots

--- R/001_censoring_plot.R
+++ R/001_censoring_plot.R
@@ -6,7 +6,8 @@ calendar <- seq(as.Date(Sys.Date()-100), as.Date(Sys.Date()), by = "day")
 size <- 12
 events <- c('Censoring', 'Event')
 
-data.frame(
+survdata <- 
+  data.frame(
   id = 1:size,
   start_date = sample(calendar, size = size),
     end_date = sample(calendar, size = size),
@@ -21,7 +22,9 @@ data.frame(
           df$start_date[!when] <- df$end_date[!when]
             df$end_date[!when] <-.$start_date[!when]
   df
-} %>% {
+}
+
+survdata %>% {
   dt <- .
   dt %>%
   ggplot(aes(ymin = start_date, ymax = end_date, x = id)) +
@@ -46,3 +49,35 @@ data.frame(
     ) 
 }
 g6tr_save('plots/001_censoring.pdf')
+
+
+
+
+survdata %>%
+  mutate(time = difftime(end_date, start_date, units = "days")) %>%
+  {
+    dt <- .
+    dt %>%
+      ggplot(aes(ymin = 0, ymax = time, x = id)) +
+      geom_linerange(size = 2, colour = g6tr_pal(palette = 'core')(2)[2]) +
+      coord_flip() +
+      geom_point(aes(x = id, y = time, shape = event),
+                 size = 3,
+                 colour = g6tr_pal(palette = 'core')(3)[3]) +
+      scale_x_continuous(labels = 1:size, breaks = 1:size) +
+      # scale_y_date(breaks = seq(as.Date(min(dt$start_date)),
+      #                           as.Date(max(dt$end_date)),
+      #                           by = 'month')) +
+      labs(x = "Observation", y = "Days") +
+      guides(
+        color  = guide_legend(override.aes = list(fill = NA)),
+        shape  = guide_legend(title = 'Type of the event')
+      ) +
+      theme(axis.text.x = element_text(family = 'Anonymous Pro', angle = 0, hjust = 0.5),
+            panel.grid.minor.y = element_blank(),
+            legend.background = element_rect(fill = '#DDDDDA', colour = NA),
+            legend.box.background = element_rect(colour = '#DDDDDA', fill = '#DDDDDA')
+      ) 
+  }
+
+g6tr_save('plots/001_censoring_normalized.pdf')
